a758c9e64236664277d7ef54d75ced2eb00026ba
yehan_zhou <yehan_zhou@kingdee.com>
2022-03-04 18:02:57 +0800

    code: parseParam

--- js-hand-writing/parseParam.js
+++ js-hand-writing/parseParam.js
@@ -1,3 +1,22 @@
 function parseParam(url) {
-    
+    const paramsStr = /.+\?(.+)$/.exec(url)[1]
+    const paramsArr = paramsStr.split('&')
+    const paramsObj = {}
+    paramsArr.forEach(param => {
+        if(/=/.test(param)) {
+            let [key, val] = param.split('=')
+            val = decodeURIComponent(val)
+            val = /^\d+$/.test(val) ? parseFloat(val) : val
+            if(paramsObj.hasOwnProperty(key)) {
+                paramsObj[key] = [].concat(paramsObj[key], val)
+            } else {
+                paramsObj[key] = val
+            }
+        } else {
+            return paramsObj[param]=true
+        }
+    })
+    return paramsObj
 }
+
+console.log(parseParam('baidu.com?x=1&x=2&y=3'))
